2232a25994f4830a9bc863dd7e741f5eb6a99429
Pietari Puranen <Pietari.Puranen@lut.fi>
2021-02-19 16:23:25 +0200

    -Changed naming convention "opencircuit"->"nernst". -Moved Nernst equation pressure correction calculation to "nernst_pressure_correction.m" (Not tested yet!)

---
+++ nernst.m
@@ -0,0 +1,45 @@
+% Nernst equation for open circuit voltage
+% Inputs:   T - Measured temperature
+%           p1 - Parameter 1: 
+%               for "PEM" cathode pressure, in bar
+%               for "alkaline" system pressure, in bar
+%           p2 - Parameter 2: 
+%               for "PEM" anode pressure, in bar
+%               for "alkaline" electrolyte molality, in mol/kg of solvent
+%           type - Electrolysis type, "PEM" or "alkaline"
+%           model - Used reversible potential model reference, numeric, from which article
+%           electrolyte - Electrolyte used for "alkaline"
+
+function Uocv = nernst(T,p1,p2,varargin)
+    
+    defaultModel = 6;
+    defaultElectrolyte = 'KOH';
+
+    parser = inputParser;
+    addRequired(parser,'T',@(x) isnumeric(x));
+    addRequired(parser,'p1',@(x) isnumeric(x));
+    addRequired(parser,'p2',@(x) isnumeric(x));
+    addParameter(parser,'type',@(x) ischar(x)||isstring(x));
+    addParameter(parser,'model',defaultModel,@(x) isnumeric(x)&&isscalar(x))
+    addParameter(parser,'electrolyte',defaultElectrolyte,@(x) ischar(x)||isstring(x))
+    
+    parse(parser,T,p1,p2,varargin{:});
+    
+    type = lower(parser.Results.type);
+    model = parser.Results.model;
+    electrolyte = parser.Results.electrolyte;
+    
+    %% Errors
+    if type == "alkaline"
+        if electrolyte ~= "KOH"||electrolyte ~= "NaOH"
+            error('Only KOH and NaOH defined as alkaline electrolytes')
+        end
+    elseif type ~= "pem"
+        error('Only PEM and alkaline electrolysis defined for Nernst equation.')
+    end
+    
+    %% Nernst equation
+    
+    Uocv = reversible(T,model) + nerns_pressure_correction(T,p1,p2,type,electrolyte);
+    
+end
---
+++ nernst_pressure_correction.m
@@ -0,0 +1,60 @@
+% Pressure correction for Nernst equation
+% Inputs:   T - Measured temperature
+%           p1 - Parameter 1: 
+%               for "PEM" cathode pressure
+%               for "alkaline" system pressure
+%           p2 - Parameter 2: 
+%               for "PEM" anode pressure
+%               for "alkaline" electrolyte molality
+%           type - Electrolysis type, "PEM" or "alkaline"
+%           electrolyte - Electrolyte used for "alkaline"
+
+function Ucor = nernst_pressure_correction(T,p1,p2,varargin)
+    
+    defaultElectrolyte = 'KOH';
+
+    parser = inputParser;
+    addRequired(parser,'T',@(x) isnumeric(x));
+    addRequired(parser,'p1',@(x) isnumeric(x));
+    addRequired(parser,'p2',@(x) isnumeric(x));
+    addParameter(parser,'type',@(x) ischar(x)||isstring(x));
+    addParameter(parser,'electrolyte',defaultElectrolyte,@(x) ischar(x)||isstring(x))
+    
+    parse(parser,T,p1,p2,varargin{:});
+    
+    type = lower(parser.Results.type);
+    electrolyte = parser.Results.electrolyte;
+    
+    %% Errors
+    if type == "alkaline"
+        if electrolyte ~= "KOH"||electrolyte ~= "NaOH"
+            error('Only KOH and NaOH defined as alkaline electrolytes')
+        end
+    elseif type ~= "pem"
+        error('Only PEM and alkaline electrolysis defined for Nernst equation.')
+    end
+    %%
+    global F n_e R;
+
+    %%
+    
+    psv = antoine(T); % TODO!
+    
+    switch type
+        case "pem"
+            pH2 = p1 - psv; % bar, Hydrogen partial pressure
+            pO2 = p2 - psv; % bar, Oxygen partial pressure
+            
+            % Nernst equation pressure correction
+            Ucor = (R.*T)/(n_e*F)*log(pH2.*pO2.^(1/2));
+            
+        case "alkaline"
+            p = p1; % bar, System pressure
+            m = p2; % mol/kg of solvent, Molality of the electrolyte
+            
+            [psvEl,aH2OEl] = electrolyte_parameters(T,m,electrolyte); % TODO!
+            
+            % Nernst equation pressure correction
+            Ucor = (R.*T)/(n_e*F)*log((p - psvEl).^(3/2)./aH2OEl);
+    end
+end
