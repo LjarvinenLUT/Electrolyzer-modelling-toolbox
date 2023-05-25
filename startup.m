9f773338d538688b8149bd9a0feb9fb7440a22f8
Pietari Puranen <Pietari.Puranen@lut.fi>
2023-05-25 14:41:15 +0300

    Addition to startup for cleaning the table

--- startup.m
+++ startup.m
@@ -6,6 +6,10 @@
 % in a directory containing this file. (Other commands that should be run
 % when opening the project can be added in the end of this file.)
 
+close all;
+clearvars;
+clc;
+
 toolpath = pwd; 
          %  ^       Change the variable toolpath to a character vector 
          %  |       or a string pointing to the directory where the
